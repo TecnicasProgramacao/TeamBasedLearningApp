7457b53c4f94c6312ec9041c7a5a2ff79768394a
Tulio Coelho <tulioopc@gmail.com>
2017-04-01 14:55:52 -0300

    Adding fake data to enable all US sprint

--- client/components/app.js
+++ client/components/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import QuestionList from '../containers/questionList';
 
 const App = () => {
   return (
     <div>
-      <h1>Testando Travis</h1>
+      <QuestionList />
     </div>
   );
 };
---
+++ client/reducers/reducerFakeData.js
@@ -0,0 +1,51 @@
+export default function () {
+  return [
+    {
+      enable: true,
+      questions: [
+        {
+          description: 'De acordo com o modelo de requisitos do SAFe, um item de backlog pode ser, EXCETO:',
+          alternatives: [
+            {
+              alternativeDescription: 'Épico',
+              alternativePoints: 0,
+            },
+            {
+              alternativeDescription: 'Capacidade(Capability)',
+              alternativePoints: 4,
+            },
+            {
+              alternativeDescription: 'Requisitos Não funcionais',
+              alternativePoints: 0,
+            },
+            {
+              alternativeDescription: 'Feature',
+              alternativePoints: 0,
+            },
+          ],
+        },
+        {
+          description: 'Sobre o nível de Portifólio, é INCORRETO afirmar:',
+          alternatives: [
+            {
+              alternativeDescription: 'Os épicos do portifolio são elementos que permitem entender como os ARTs são gerenciados no SAFe.',
+              alternativePoints: 4,
+            },
+            {
+              alternativeDescription: 'Os épicos representam grandes iniciativas da organização.',
+              alternativePoints: 0,
+            },
+            {
+              alternativeDescription: 'Cada épico requer análise de custo, impacto e oportunidade.',
+              alternativePoints: 0,
+            },
+            {
+              alternativeDescription: 'Os épicos são aprovados antes de serem implementados.',
+              alternativePoints: 0,
+            },
+          ],
+        },
+      ],
+    },
+  ];
+}
