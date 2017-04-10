03e21871382b26b7326603d7acdaa90338db3d19
Victor Arnaud <victorhad@gmail.com>
2017-04-10 10:49:55 -0300

    Create login system component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import AccountsUI from './accounts.js';
 
 class NavBar extends Component {
 
@@ -7,16 +8,18 @@ class NavBar extends Component {
     return (
       <nav className="teal darken-4">
         <div className="nav-wrapper">
-          <Link to="/" className="brand-logo home-brand">
+          <Link to="/" className="center brand-logo home-brand">
             <i className="material-icons">home</i>
             Team Based Learn
           </Link>
           <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-          <ul className="right hide-on-med-and-down">
+          <ul className="left hide-on-med-and-down">
+            <li><AccountsUI /></li>
             <li><Link to="/group-simulator">Prova em grupo</Link></li>
             <li><Link to="/individual-simulator">Prova individual</Link></li>
           </ul>
           <ul className="side-nav" id="mobile-demo">
+            <li><AccountsUI /></li>
             <li><Link to="/group-simulator">Prova em grupo</Link></li>
             <li><Link to="/individual-simulator">Prova individual</Link></li>
           </ul>
