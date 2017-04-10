f7039df7ee75a3bae4aec1784b4faa486b376b91
Victor Arnaud <victorhad@gmail.com>
2017-04-09 23:48:07 -0300

    Create navbar component and insert accounts plugin

--- client/components/app.js
+++ client/components/app.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import QuestionList from '../containers/questionList';
+import NavBar from './navbar';
 
-const App = () => {
-  return (
-    <div>
-      <QuestionList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div id="main" className="container">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default App;
---
+++ client/components/navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class NavBar extends Component {
+
+  render() {
+    return (
+      <nav className="teal darken-4">
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo home-brand">
+            <i className="material-icons">home</i>
+            Team Based Learn
+          </Link>
+          <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+          <ul className="right hide-on-med-and-down">
+            <li><Link to="/group-simulator">Prova em grupo</Link></li>
+            <li><Link to="/individual-simulator">Prova individual</Link></li>
+          </ul>
+          <ul className="side-nav" id="mobile-demo">
+            <li><Link to="/group-simulator">Prova em grupo</Link></li>
+            <li><Link to="/individual-simulator">Prova individual</Link></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
